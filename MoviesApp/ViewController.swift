2df05da68945528a5887f7f64f0fabfc8e4c4cfe
amr zayed <amrzayed22@gmail.com>
2020-10-21 02:45:19 +0200

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by A on 21/10/2020.
+//  Copyright © 2020 amr zayed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
